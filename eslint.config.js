356efa05a6936437899499b373e64ab83bd75b34
Nikita Almanov <131481562+nikkeyl@users.noreply.github.com>
2024-02-01 13:24:57 +0300

    refactor(*rc): rename to `*.config.js`

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+export default {
+  root: true,
+  env: {
+    browser: true,
+    es6: true,
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  extends: ['airbnb-base', 'plugin:unicorn/recommended'],
+  plugins: ['import', 'unicorn'],
+  rules: {
+    'import/exports-last': 'error',
+    'import/group-exports': 'warn',
+    'import/no-commonjs': 'error',
+    'import/no-namespace': 'error',
+    'import/no-unassigned-import': 'error',
+    'unicorn/no-unused-properties': 'error',
+    'unicorn/string-content': 'error',
+  },
+};
